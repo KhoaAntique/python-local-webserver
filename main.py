6cad2cf69f9c7bf551e36853ce94822bf733b9ad
KhoaAntique <khoanguyen359@gmail.com>
2023-06-30 13:36:01 -0500

    💩 First Coommit

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+from http.server import HTTPServer
+from server import Server
+
+#HTTPServer is a subclass of socketserver.TCPServer that creates and listens for HTTP sockets, sends requests to the processor
+HOST_NAME = 'localhost'
+PORT = 8000
+
+if __name__ == "__main__":
+    httpd = HTTPServer((HOST_NAME,PORT),Server)
+    print(time.asctime(), "Start Server - %s:%s"%(HOST_NAME,PORT))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print(time.asctime(),'Stop Server - %s:%s' %(HOST_NAME,PORT))
---
+++ server.py
@@ -0,0 +1,23 @@
+from http.server import BaseHTTPRequestHandler
+import os
+
+# BaseHTTPRequestHandler is used to handle HTTP requests to the server
+class Server(BaseHTTPRequestHandler):
+    #do_GET() runs when there is a GET request sent
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        try:
+            split_path = os.path.splitext(self.path)
+            request_extension = split_path[1]
+            if request_extension != ".py":
+                f = open(self.path[1:]).read()
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(bytes(f, 'utf-8'))
+            else:
+                f = "File not found"
+                self.send_error(404,f)
+        except:
+            f = "File not found"
+            self.send_error(404,f)
